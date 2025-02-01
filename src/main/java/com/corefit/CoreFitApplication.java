f1ff59c5e5761f5ee82bd6848c39800461027c5a
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-02-01 16:40:58 +0200

    git repo

---
+++ src/main/java/com/corefit/CoreFitApplication.java
@@ -0,0 +1,13 @@
+package com.corefit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreFitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/corefit/CoreFitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.corefit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreFitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
