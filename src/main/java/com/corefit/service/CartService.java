2ad254f12c1fb080254b519dd94ce59b1efde7ea
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-02-18 19:52:14 +0000

    fix cart get service and products controller naming

--- src/main/java/com/corefit/service/CartService.java
+++ src/main/java/com/corefit/service/CartService.java
@@ -6,13 +6,14 @@ import com.corefit.dto.GeneralResponse;
 import com.corefit.entity.Cart;
 import com.corefit.entity.User;
 import com.corefit.exceptions.GeneralException;
-import com.corefit.repository.CartItemRepo;
 import com.corefit.repository.CartRepo;
 import com.corefit.repository.UserRepo;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -25,6 +26,7 @@ public class CartService {
     @Autowired
     private UserRepo userRepo;
 
+    @Transactional
     public GeneralResponse<?> getCart(HttpServletRequest httpRequest) {
         String userId = authService.extractUserIdFromRequest(httpRequest);
         Long userIdLong = Long.parseLong(userId);
@@ -35,12 +37,12 @@ public class CartService {
         Cart cart = cartRepo.findByUserId(userIdLong);
 
         if (cart == null) {
-            Cart newCart = new Cart();
+            cart = new Cart();
             cart.setUser(user);
-            cart = cartRepo.save(newCart);
+            cart = cartRepo.save(cart);
         }
 
-        List<CartItemDto> cartItemDtos = cart.getCartItems().stream()
+        List<CartItemDto> cartItemDtos = cart.getCartItems() != null ? cart.getCartItems().stream()
                 .map(item -> new CartItemDto(
                         item.getProduct().getId(),
                         item.getProduct().getName(),
@@ -50,7 +52,8 @@ public class CartService {
                         item.getProduct().getSubCategory().getName(),
                         item.getProduct().getImages(),
                         item.getQuantity()
-                )).toList();
+                )).toList() : Collections.emptyList();
+
 
         double totalPrice = cartItemDtos.stream().mapToDouble(CartItemDto::getTotal).sum();
 
@@ -63,7 +66,4 @@ public class CartService {
         return new GeneralResponse<>("Success", cartDto);
     }
 
-//    private GeneralResponse<?> insertCart(Cart cart) {
-//        return new GeneralResponse<>("cart added successfully", cart);
-//    }
 }
