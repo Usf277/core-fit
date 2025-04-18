adc412f5e985a925bddf64d609ff37c30c8204eb
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2025-04-18 20:52:33 +0200

    production fix

--- src/main/java/com/corefit/repository/GovernorateRepo.java
+++ src/main/java/com/corefit/repository/GovernorateRepo.java
@@ -4,7 +4,9 @@ import com.corefit.entity.Governorate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface GovernorateRepo extends JpaRepository<Governorate, Long> {
 
+    public Governorate findById(long id);
 }
